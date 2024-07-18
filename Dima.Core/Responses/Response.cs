322124e5fe4230477b8eee5239e3cac4a73c09e7
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-18 09:42:35 -0300

    feat: add response class.

---
+++ Dima.Core/Responses/Response.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses
+{
+    public class Response<T>
+    {
+        private const int DefaultStatusCode = 200;
+        private readonly int _code = DefaultStatusCode;
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonConstructor]
+        public Response() => _code = DefaultStatusCode;
+
+        public Response(T? data, int code = DefaultStatusCode, string? message = null)
+        {
+            Data = data;
+            Message = message;
+            _code = code;
+        }
+
+        [JsonIgnore]
+        public bool IsSuccess => _code is >= 200 and < 300;
+    }
+}
