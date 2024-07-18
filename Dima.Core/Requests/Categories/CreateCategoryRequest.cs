ea8bc2af85318c4e5470ad8ba474a16263e805e6
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-18 17:03:16 -0300

    feat: add categories requests.

---
+++ Dima.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Categories
+{
+    public class CreateCategoryRequest : BaseRequest
+    {
+        [Required(ErrorMessage = "Título inválido")]
+        [MaxLength(80, ErrorMessage = "Título deve conter no máximo 80 caracteres")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Descrição inválida")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
