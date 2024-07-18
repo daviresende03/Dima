2c285914b3b63819ddcc81321ce82b77128f5f4e
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-18 09:12:58 -0300

    Initial commit

---
+++ Dima.Api/Program.cs
@@ -0,0 +1,28 @@
+using Dima.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder
+    .Configuration
+    .GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(connectionString);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(y => y.FullName);
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dima.Core.Requests
+{
+    public abstract class PagedRequest : BaseRequest
+    {
+        public int PageNumber { get; set; } = 2;
+        public int PageSize { get; set; }
+    }
+}
