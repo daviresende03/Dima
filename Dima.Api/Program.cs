b707f799781549bdd609066339a6a1f7bf728a1f
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-19 09:50:31 -0300

    feat: create endpoints for category.

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,4 +1,9 @@
 using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,11 +23,84 @@ builder.Services.AddSwaggerGen(x =>
     x.CustomSchemaIds(y => y.FullName);
 });
 
+builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/v1/categories",
+    async ( ICategoryHandler handler ) => {
+        var request = new GetAllCategoriesRequest
+        {
+            //UserId = 
+        };
+        return await handler.GetAllAsync(request);
+    }
+)
+.WithName("Categories: Get All")
+.WithSummary("Search a list of categories")
+.Produces<Response<Category?>>();
+
+app.MapGet("/v1/categories/{id}",
+    async (
+        long id,
+        ICategoryHandler handler
+    ) => {
+        var request = new GetCategoryByIdRequest
+        {
+            Id = id,
+            //UserId = 
+        };
+        return await handler.GetByIdAsync(request);
+    }
+)
+.WithName("Categories: Get by Id")
+.WithSummary("Search an existing category")
+.Produces<Response<Category?>>();
+
+app.MapPost( "/v1/categories", 
+    async (
+        CreateCategoryRequest request, 
+        ICategoryHandler handler 
+    ) => await handler.CreateAsync(request)
+)
+.WithName("Categories: Create")
+.WithSummary("Create a new category")
+.Produces<PagedResponse<List<Category>?>>();
+
+app.MapPut("/v1/categories/{id}", 
+    async (
+        long id,
+        UpdateCategoryRequest request,
+        ICategoryHandler handler
+    ) => {
+        request.Id = id;
+        return await handler.UpdateAsync(request);
+    }
+)
+.WithName("Categories: Update")
+.WithSummary("Update an existing category")
+.Produces<Response<Category?>>();
+
+app.MapDelete("/v1/categories/{id}", 
+    async (
+        long id,
+        ICategoryHandler handler
+    ) => {
+        var request = new DeleteCategoryRequest
+        {
+            Id = id,
+            //UserId = 
+        };
+        return await handler.DeleteAsync(request);
+    }
+)
+.WithName("Categories: Delete")
+.WithSummary("Delete an existing category")
+.Produces<Response<Category?>>();
 
 app.Run();
