fb52c42cabf92b565c851e046e8b25936a27eabb
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-19 12:55:09 -0300

    fix: standardization of category endpoints.

---
+++ Dima.Api/Endpoints/BaseEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Api.Endpoints.Categories;
+
+namespace Dima.Api.Endpoints
+{
+    public static class BaseEndpoint
+    {
+        public static void MapEndpoints(this WebApplication app)
+        {
+            var endpoints = app
+                .MapGroup("");
+
+            endpoints.MapGroup("v1/categories")
+                .WithTags("Categories")
+                .RequireAuthorization()
+                .MapEndpoint<CreateCategoryEndpoint>()
+                .MapEndpoint<UpdateCategoryEndpoint>()
+                .MapEndpoint<DeleteCategoryEndpoint>()
+                .MapEndpoint<GetCategoryByIdEndpoint>()
+                .MapEndpoint<GetAllCategoriesEndpoint>();
+        }
+
+        private static IEndpointRouteBuilder MapEndpoint<T>(this IEndpointRouteBuilder app) 
+            where T : IEndpoint
+        {
+            T.Map(app);
+            return app;
+        }
+    }
+}
---
+++ Dima.Api/Endpoints/Categories/GetAllCategoriesEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Dima.Api.Common.Api;
+using Dima.Core;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class GetAllCategoriesEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+            => app.MapGet("/{pageSize}/{pageNumber}", HandleAsync)
+            .WithName("Categories: Get All")
+            .WithSummary("Search a list of categories")
+            .WithOrder(5)
+            .Produces<Response<List<Category>?>>();
+
+        private static async Task<IResult> HandleAsync(
+            ICategoryHandler handler,
+            [FromQuery] int pageNumber = Configuration.DefaultPageNumber,
+            [FromQuery] int pageSize = Configuration.DefaultPageSize)
+        {
+            var request = new GetAllCategoriesRequest
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                //UserId = 
+            };
+            
+            var result = await handler.GetAllAsync(request);
+
+            return result.IsSuccess ?
+                TypedResults.Ok(result) :
+                TypedResults.BadRequest(result);
+        }
+    }
+}
