7783cb450e287f1795b63553b8725938c48e5cdf
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-22 11:03:39 -0300

    feat: transaction handler.

---
+++ Dima.Api/Handlers/TransactionHandler.cs
@@ -0,0 +1,135 @@
+﻿using Dima.Api.Data;
+using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
+using Dima.Core.Responses;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Dima.Api.Handlers
+{
+    public class TransactionHandler(AppDbContext context) : ITransactionHandler
+    {
+        public async Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest request)
+        {
+            try
+            {
+                var transaction = new Transaction
+                {
+                    UserId = request.UserId,
+                    Title = request.Title,
+                    CreatedAt = DateTime.Now,
+                    Amount = request.Amount,
+                    PaidOrReceivedAt = request.PaidOrReceivedAt,
+                    CategoryId = request.CategoryId,
+                    Type = request.Type
+                };
+
+                await context.Transactions.AddAsync(transaction);
+                await context.SaveChangesAsync();
+
+                return new Response<Transaction?>(transaction, (int)HttpStatusCode.Created, "Transaction successfully created");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Transaction?>(null, (int)HttpStatusCode.InternalServerError, "An error occurred while creating a transaction");
+            }
+        }
+
+        public async Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest request)
+        {
+            try
+            {
+                var transaction = await context.Transactions.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+                if (transaction is null)
+                    return new Response<Transaction?>(null, (int)HttpStatusCode.NotFound, "Transaction not found");
+
+                context.Transactions.Remove(transaction);
+                await context.SaveChangesAsync();
+
+                return new Response<Transaction?>(transaction, message: "Transaction successfully deleted");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Transaction?>(null, (int)HttpStatusCode.InternalServerError, "An error occurred while deleting a transaction");
+            }
+        }
+
+        public async Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest request)
+        {
+            try
+            {
+                var transaction = await context.Transactions
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+                return transaction is null ?
+                    new Response<Transaction?>(null, (int)HttpStatusCode.NotFound, "Transaction not found") :
+                    new Response<Transaction?>(transaction);
+            }
+            catch (Exception ex)
+            {
+                return new Response<Transaction?>(null, (int)HttpStatusCode.InternalServerError, "An error occurred while searching a transaction");
+            }
+        }
+
+        public async Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionsByPeriodRequest request)
+        {
+            try
+            {
+                request.StartDate ??= DateTime.Now.GetFirstDay();
+                request.EndDate ??= DateTime.Now.GetLastDay();
+
+                var query = context.Transactions
+                    .AsNoTracking()
+                    .Where(x => 
+                        x.CreatedAt >= request.StartDate && 
+                        x.CreatedAt <= request.EndDate && 
+                        x.UserId == request.UserId)
+                    .OrderBy(x => x.CreatedAt);
+
+                var transactions = await query
+                    .Skip((request.PageNumber - 1) * request.PageSize)
+                    .Take(request.PageSize)
+                    .ToListAsync();
+
+                var count = await query.CountAsync();
+
+                return new PagedResponse<List<Transaction>?>(transactions, count, request.PageNumber, request.PageSize);
+            }
+            catch (Exception ex)
+            {
+
+               return new PagedResponse<List<Transaction>?>(null, (int)HttpStatusCode.InternalServerError, "An error occurred while searching transactions");
+            }
+        }
+
+        public async Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest request)
+        {
+            try
+            {
+                var transaction = await context.Transactions.FirstOrDefaultAsync(x => x.Id == request.Id && x.UserId == request.UserId);
+
+                if (transaction is null)
+                    return new Response<Transaction?>(null, (int)HttpStatusCode.NotFound, "Transaction not found");
+
+                transaction.Title = request.Title;
+                transaction.Amount = request.Amount;
+                transaction.PaidOrReceivedAt = request.PaidOrReceivedAt;
+                transaction.CategoryId = request.CategoryId;
+                transaction.Type = request.Type;
+
+                context.Transactions.Update(transaction);
+                await context.SaveChangesAsync();
+
+                return new Response<Transaction?>(transaction, message: "Transaction successfully updated");
+            }
+            catch (Exception ex)
+            {
+                return new Response<Transaction?>(null, (int)HttpStatusCode.InternalServerError, "An error occurred while updating a transaction");
+            }
+        }
+    }
+}
