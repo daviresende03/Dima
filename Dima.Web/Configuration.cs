778582f69941319767091a1a1d2aa0043a2f6fa0
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-23 15:05:11 -0300

    feat: frontend login handler.

--- Dima.Web/Configuration.cs
+++ Dima.Web/Configuration.cs
@@ -4,6 +4,9 @@ namespace Dima.Web
 {
     public static class Configuration
     {
+        public const string HttpClientName = "Dima.ServerAPI";
+        public static string BackendUrl { get; set; } = "http://localhost:5291";
+
         public static MudTheme Theme = new()
         {
             Typography = new Typography
---
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,29 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers
+{
+    public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+    {
+        private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+        public async Task<Response<string>> LoginAsync(LoginRequest request)
+        {
+            var result = await _client.PostAsJsonAsync("v1/identity/login?useCookies=true", request);
+            return result.IsSuccessStatusCode ?
+                new Response<string>("You are successfully logged in") :
+                new Response<string>(null, (int)result.StatusCode, "Username or password incorrect");
+        }
+
+        public Task LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<string>> RegisterAsync(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
