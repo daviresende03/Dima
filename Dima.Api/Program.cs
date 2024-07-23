69560e17ef8c6411f77e848d99583e1e53e49618
daviresende03 <107009159+daviresende03@users.noreply.github.com>
2024-07-23 09:07:51 -0300

    feat: endpoint to logout user and get user roles.

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -5,6 +5,7 @@ using Dima.Api.Models;
 using Dima.Core.Handlers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -45,5 +46,43 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "Ok");
 app.MapEndpoints();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout",
+        async (SignInManager<User> signInManager) =>
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    })
+    .RequireAuthorization();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapGet("/roles",
+        (ClaimsPrincipal user) =>
+        {
+            if (user.Identity is null || !user.Identity.IsAuthenticated)
+                return Results.Unauthorized();
+
+            var identity = (ClaimsIdentity)user.Identity;
+            var roles = identity
+            .FindAll(identity.RoleClaimType)
+            .Select(x => new
+            {
+                x.Issuer,
+                x.OriginalIssuer,
+                x.Type,
+                x.Value,
+                x.ValueType
+            });
+            
+            return Results.Json(roles);
+        })
+    .RequireAuthorization();
 
 app.Run();
